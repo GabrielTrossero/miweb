673235f9418291a883c0ad02642f71ad053b32b1
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2019-02-11 10:38:41 -0300

    parte2-ejercicio2

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -1,4 +1,31 @@
 @extends('layouts.master')
+
+
 @section('content')
-  Vista detalle pelicula {id}
+
+<div class="row">
+  <div class="col-sm-4">
+    {{ $pelicula['poster'] }}
+  </div>
+  <div class="col-sm-8">
+    <h2> {{ $pelicula['title'] }} </h2>
+    <h5><strong>Año:</strong> {{ $pelicula['year'] }} </h5>
+    <h5><strong>Director:</strong> {{ $pelicula['director'] }} </h5>
+    <br>
+    <strong> <u>Resumen:</u></strong> {{ $pelicula['synopsis'] }}
+    <br>
+    <strong> <u>Estado:</u></strong>
+    @if($pelicula['rented'])
+      Pelicula actualmente alquilada
+      <br><br>
+      <button type="button" class="btn btn-danger">Devolver</button>
+    @else
+      Pelicula disponible
+      <br><br>
+      <button type="button" class="btn btn-primary">Alquilar</button>
+    @endif
+  </div>
+</div>
+
+
 @stop
